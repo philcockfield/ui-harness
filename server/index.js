1be4fd7c9eda8467894b413e8098cc2a071b92b4
Phil Cockfield <phil@cockfield.net>
2015-07-04 16:28:36 -0700

    Move dev-server code directly into the `ui-harness` module.

---
+++ server/index.js
@@ -0,0 +1,56 @@
+import _ from 'lodash';
+import chalk from 'chalk';
+import express from 'express';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import immutable from 'immutable';
+import * as config from './webpack.config'
+
+const PORT = config.PORT;
+
+
+
+/*
+Starts the UIHarness within a development server.
+@params options: Configuration options.
+
+          - entry: A string or array of strings to entry points of files
+                   to pass to WebPack to build for the client.
+
+*/
+export var start = (options = {}) => {
+  var app = express();
+
+  // Prepare configuration.
+  var webpackOptions = immutable.fromJS(config.compiler).toJS();
+  var entry = options.entry || [];
+  if (!_.isArray(entry)) entry = [entry];
+  entry.forEach((path) => webpackOptions.entry.push(path));
+
+  // Create the WebPack compiler and hot-reloading dev server.
+  var compiler = webpack(webpackOptions);
+  app.use(webpackMiddleware(compiler, config.options));
+  app.use(webpackHotMiddleware(compiler));
+
+
+  // Serve host HTML page from root.
+  app.get('/', function (req, res) { res.sendFile(`${ __dirname }/index.html`) });
+
+
+  // Serve JSON.
+  app.get('/json', function (req, res) {
+    res.json({ text:'Hello World' });
+  });
+
+
+
+  // Start the server. -----------------------------------------------------------
+  //
+  var server = app.listen(PORT, (err, result) => {
+        const HR = chalk.cyan(_.repeat('-', 80));
+        console.log(HR);
+        console.log(chalk.grey('UIHarness running on'), chalk.cyan(`localhost:${ PORT }`));
+        console.log(HR, '\n');
+  });
+};
---
+++ server/webpack.config.js
@@ -0,0 +1,56 @@
+/*
+See:
+
+    https://github.com/webpack/webpack-dev-middleware
+    https://www.npmjs.com/package/webpack-hot-middleware
+
+*/
+import webpack from 'webpack';
+import fsPath from 'path';
+
+export const PORT = 8080;
+const NODE_MODULES_PATH = fsPath.join(__dirname, '../node_modules');
+
+
+export var compiler = {
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-hot-middleware/client',
+    fsPath.join(__dirname, '../components/index.js')
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: '/',
+    publicPath: `http://localhost:${ PORT }/public`
+  },
+
+  plugins: [
+    // Hot reload plugins:
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  resolve: { fallback : NODE_MODULES_PATH },
+  resolveLoader: { fallback: NODE_MODULES_PATH },
+
+  module: {
+    loaders: [
+      // ES2015 (ES6).
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+    ]
+  }
+};
+
+
+export var options = {
+    noInfo: false,
+    quiet: false,
+    lazy: false,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: true
+    },
+    publicPath: compiler.output.publicPath,
+    stats: { colors: true }
+};
