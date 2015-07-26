31d47a76084b2abe3519bfd14c337d6af55ecf98
Phil Cockfield <phil@cockfield.net>
2015-07-27 08:53:29 +1200

    Lib building pattern.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
-'use strict'
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
+"use strict"
+var gulp = require("gulp");
+var eslint = require("gulp-eslint");
+var babel = require("gulp-babel");
+var SOURCE_PATH = ["./src/**/*.js", "./src/**/*.jsx"];
 
 
-gulp.task('lint', function() {
-  return gulp.src(['./src/**/*.js', './src/**/*.jsx'])
+gulp.task("build", function () {
+  return gulp.src(SOURCE_PATH)
+    .pipe(babel())
+    .pipe(gulp.dest("lib"));
+});
+gulp.task("watch", function(callback) { gulp.watch(SOURCE_PATH, ["build"]) });
+
+
+
+
+gulp.task("lint", function() {
+  return gulp.src(SOURCE_PATH)
     .pipe(eslint())
     .pipe(eslint.format());
 });
 
 
-// ----------------------------------------------------------------------------
-gulp.task('default', ['lint']);
+
+gulp.task("default", ["build", "watch"]);
--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-'use strict'
-require('babel/register');
-module.exports = require('./src/server');
+"use strict"
+require("babel/register");
+module.exports = require("./src/server");
