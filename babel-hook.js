734e479f6db4b737de95025a6faf87ca777a990e
Phil Cockfield <phil@cockfield.net>
2015-08-10 17:05:07 +1200

    Babel hook

---
+++ babel-hook.js
@@ -0,0 +1,7 @@
+"use strict"
+require("babel/register")({
+  stage: 1  // Experimental:level-1
+            // Allows for @decorators
+            // See: http://babeljs.io/docs/usage/experimental/
+            // For example, used by @Radium (CSS)
+});
--- server.js
+++ server.js
@@ -1,8 +1,3 @@
 "use strict"
-require("babel/register")({
-  stage: 1  // Experimental:level-1
-            // Allows for @decorators
-            // See: http://babeljs.io/docs/usage/experimental/
-            // For example, used by @Radium (CSS)
-});
+require("./babel-hook");
 module.exports = require("./src/server");
