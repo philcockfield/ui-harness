4d89fe74bd431372c5c37e963b11ec54d13b455a
Phil Cockfield <phil@cockfield.net>
2015-09-07 16:16:30 +1200

    Publish /src to NPM.

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,6 @@ var babel = require("gulp-babel");
 var SOURCE_PATH = ["./src/**/*.js", "./src/**/*.jsx"];
 
 
-// gulp.task("build", function () {
-//   return gulp.src(SOURCE_PATH)
-//     .pipe(babel())
-//     .pipe(gulp.dest("lib"));
-// });
-// gulp.task("watch", function(callback) { gulp.watch(SOURCE_PATH, ["build"]) });
-
-
-
-
 gulp.task("lint", function() {
   return gulp.src(SOURCE_PATH)
     .pipe(eslint())
