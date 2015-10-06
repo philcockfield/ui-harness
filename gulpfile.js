a2525aad58e8f8aa7f9aebcb04a8a92a0cae02cc
Phil Cockfield <phil@cockfield.net>
2015-10-06 12:28:23 -0700

    Keep gulp build task alive

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,30 @@
 "use strict"
 var gulp = require("gulp");
+var plumber = require("gulp-plumber");
 var eslint = require("gulp-eslint");
 var babel = require("gulp-babel");
 
 var SOURCE_PATH = ["./src/**/*.js", "./src/**/*.jsx"];
 
-gulp.task("lint", function() {
-  return gulp.src(SOURCE_PATH)
-    .pipe(eslint())
-    .pipe(eslint.format());
-});
 
 
 gulp.task("build", function() {
   return gulp.src(SOURCE_PATH)
-    .pipe(babel({ stage: 1 }))
-    .pipe(gulp.dest("lib"));
+             .pipe(plumber()) // Keep task alive on build errors.
+             .pipe(babel())
+             .pipe(gulp.dest("lib"));
+});
+gulp.task("prepublish", ["build"]);
+gulp.task("watch", function(callback) { gulp.watch("./src/**/*", ["build"]) });
+
+
+
+gulp.task("lint", function() {
+  return gulp.src(SOURCE_PATH)
+             .pipe(eslint())
+             .pipe(eslint.format());
 });
 
 
-gulp.task("prepublish", ["build"]);
-gulp.task("watch", function(callback) { gulp.watch("./src/**/*", ["build"]) });
+
 gulp.task("default", ["build", "watch"]);
