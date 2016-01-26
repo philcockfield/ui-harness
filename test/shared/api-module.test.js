82efa232224e9fd35e50bf942deb05fcec8a0931
Phil Cockfield <phil@cockfield.net>
2016-01-27 12:03:31 +1300

    Remove `api-module` (close #12)

--- test/shared/api-module.test.js
+++
@@ -1,5 +0,0 @@
-import { expect } from 'chai';
-import sinon from 'sinon';
-import moduleApi from '../../src/shared/api-module';
-import api from '../../src/shared/api-internal';
-import bdd from '../../src/shared/bdd';
