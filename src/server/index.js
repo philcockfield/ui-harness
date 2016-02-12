04cc3fcdbfdad1ba47da25fc565fe8be81926bfe
Phil Cockfield <phil@cockfield.net>
2016-02-12 21:52:57 +1300

    Delete size command.

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 import 'babel-polyfill';
 import start from './start';
-import size from './size';
 import build from './build';
 
 // Server API.
-export default { start, size, build };
+export default { start, build };
