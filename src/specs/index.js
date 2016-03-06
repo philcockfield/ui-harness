4186bc9bd8c63ecd269cfda4fc745f70f0755ee4
Phil Cockfield <phil@cockfield.net>
2016-03-06 19:45:34 +1300

    CSS Module Spec (stubbed in).

--- src/specs/index.js
+++ src/specs/index.js
@@ -1,6 +1,7 @@
 describe('ui-harness', function() {
   require('./ComponentHost.spec');
   require('./css-loader.spec');
+  require('./css-module.spec');
   require('./OutputLog.spec');
   require('./header-footer.spec');
   require('./skipped.spec');
