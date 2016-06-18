e8cf2baa63bda8305020ea41407b0c1b122398d2
Frederick Fogerty <frederick.fogerty@gmail.com>
2016-06-19 10:07:57 +1200

    Remove .tsx extension from specs/index.js

--- src/specs/index.js
+++ src/specs/index.js
@@ -6,7 +6,7 @@ describe('ui-harness', function() {
   require('./it-server.spec');
   require('./PropTypes.spec');
   require('./page.spec');
-  require('./typescript.spec.tsx')
+  require('./typescript.spec')
 });
 
 // External libs.
