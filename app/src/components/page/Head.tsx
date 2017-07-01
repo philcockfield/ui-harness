715a8d09733292dc7d45b797c2e7c0fbc0f8a650
Phil Cockfield <phil@cockfield.net>
2017-07-02 11:07:25 +1200

    Invoking node script.

--- app/src/components/page/Head.tsx
+++ app/src/components/page/Head.tsx
@@ -11,7 +11,7 @@ export const Head = (props: IHeadProps) => {
   const style = props.css && <style dangerouslySetInnerHTML={{ __html: props.css }} />;
   return (
     <next.Head>
-      <title>{props.title || 'Ooooby'}</title>
+      <title>{props.title || 'UIHarness'}</title>
       <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no' />
       <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:100,300,300i,400,400i,900' />
       <link rel='stylesheet' type='text/css' href='/css/normalize.css' />
